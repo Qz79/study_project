1f32501fc51e42de39914c5d33a9040c30e1c8ec
Qz79 <1678764240@qq.com>
2023-03-23 16:00:32 +0800

    利用简单的数据处理，模拟完成端口映射处理，理解完成端口映射基本原理，深层原理待学习

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -17,20 +17,71 @@
 // 唯一的应用程序对象
 // 001测试分支切换演示
 CWinApp theApp;
+//要传递的数据结
+enum {
+    IocpListPush,
+    IocpListPop,
+    IocpListEntry
+};
+//模拟的数据
+typedef struct IocpParam {
+    int operate;                    //对该数据进行的操作
+    std::string strData;            //数据
+    _beginthread_proc_type cbFunc;  //处理数据的回调函数
+    IocpParam(int op, std::string str, _beginthread_proc_type cb = NULL) {
+        operate = op;
+        strData = str;
+        cbFunc = cb;
+    }
+    IocpParam() {
+        operate = -1;
+    }
+}IOCP_PARAM;
+//操作完成端口对象的线程
 void threadQueueEntry(HANDLE hIocp) {
     //HANDLE hIocp==void* arg
     //该线程对hIocp的里面的数据获取
+    std::list<std::string> strList;
     DWORD dwTransferred = 0;
     ULONG_PTR CompletionKey = 0;
     OVERLAPPED* pOverLapped = NULL;
     while (GetQueuedCompletionStatus(hIocp, &dwTransferred, &CompletionKey, &pOverLapped, INFINITE)) {
-        if ((dwTransferred == 0) && (CompletionKey == NULL))
+        if ((dwTransferred == 0) || (CompletionKey == NULL))
         {
             printf("thread is prepare to exit\r\n");
             break;
         }
+        IOCP_PARAM* Param = (IOCP_PARAM*)CompletionKey;
+        if (IocpListPush == Param->operate) {
+            strList.push_back(Param->strData);
+        }
+        else if (IocpListPop == Param->operate ) {
+            std::string* str = NULL;
+            if (strList.size() > 0) {
+                str = new std::string(strList.front());
+                strList.pop_front();
+            }
+            if (NULL != Param->cbFunc) {
+                Param->cbFunc(str);
+            }
+        }
+        else if (IocpListEntry == Param->operate) {
+            strList.clear();
+        }
+        delete Param;
     }
     _endthread();
+}
+//对模拟数据处理的回调函数
+void func(void* arg) {
+    std::string* str = (std::string*)arg;
+    if (str) {
+        printf("pop from list:%s\r\n", str->c_str());
+        delete str;
+    }
+    else {
+        printf("strlist is empty,no data\r\n");
+    }
 }
 int main()
 {
@@ -43,8 +94,18 @@ int main()
     printf("press any key to exit....\r\n");
     HANDLE nThread=(HANDLE)_beginthread(threadQueueEntry, 0, hIocp);
     //4.判断完成端口对象，并向其投递信息
-    getchar();
-    while (hIocp) {
+    ULONGLONG tick = GetTickCount64();
+    while (_kbhit() != 0) {
+        if (GetTickCount64() - tick > 1300) {//每隔1.3秒触发一次
+            PostQueuedCompletionStatus(hIocp, sizeof(IOCP_PARAM), (ULONG_PTR)new IOCP_PARAM(IocpListPop, "hello world!"), NULL);
+        }
+        if (GetTickCount64() - tick > 2000) {//每隔2秒触发一次
+            PostQueuedCompletionStatus(hIocp, sizeof(IOCP_PARAM), (ULONG_PTR)new IOCP_PARAM(IocpListPush,"hello world!"), NULL);
+            tick= GetTickCount64();
+        }
+        Sleep(1);
+    }
+   if (hIocp) {
         PostQueuedCompletionStatus(hIocp, 0, NULL, NULL);
         WaitForSingleObject(nThread, INFINITE);
     }
